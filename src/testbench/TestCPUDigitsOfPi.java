0e1258d3fb2654afb935eb77eb40eb12f702190b
robertnegre <100676642+robertnegre@users.noreply.github.com>
2023-03-13 15:36:47 +0200

    lab 4

--- src/testbench/TestCPUDigitsOfPi.java
+++ src/testbench/TestCPUDigitsOfPi.java
@@ -1,7 +1,24 @@
 package testbench;
 
+import bench.*;
+import benchmark.cpu.*;
+import logging.*;
+import timing.*;
+
 public class TestCPUDigitsOfPi {
     public static void main(String[] args) {
-
+        ITimer timer = new Timer();
+        ILog log = new ConsoleLogger();
+        TimeUnit Milisecond = TimeUnit.Mili;
+        TimeUnit Microsecond = TimeUnit.Micro;
+        TimeUnit Second = TimeUnit.Sec;
+        IBenchmark bench = new CPUDigitsOfPi();
+        bench.initialize();
+        timer.start();
+        bench.run(1);
+        long time = timer.stop();
+        log.writeTime("Finished in", time, Milisecond);
+        log.close();
+        bench.clean();
     }
 }
--- src/testbench/TestDemoBench.java
+++ src/testbench/TestDemoBench.java
@@ -5,10 +5,8 @@ import logging.*;
 import timing.*;
 import bench.*;
 
-public class TestDemoBench
-{
-    public static void main(String[] args)
-    {
+public class TestDemoBench {
+    public static void main(String[] args) {
         ITimer timer = new Timer();
         ILog log = new ConsoleLogger();
         TimeUnit Milisecond = TimeUnit.Mili;
@@ -16,7 +14,7 @@ public class TestDemoBench
         TimeUnit Second = TimeUnit.Sec;
 
         IBenchmark bench = new DemoBenchmark();
-        bench.initialize(100);
+        bench.initialize(1000);
         timer.start();
         bench.run();
         long time = timer.stop();
@@ -26,14 +24,13 @@ public class TestDemoBench
         log.close();
         bench.clean();
 
-        final int workload = 100;
+        final int workload = 1000;
         bench.initialize(workload);
-        for (int i = 0; i < 12; ++i)
-        {
+        for (int i = 0; i < 12; ++i) {
             timer.resume();
             bench.run();
             long time1 = timer.pause();
-            log.write("Run " + i + " : " + time1);
+            log.writeTime("Run " + i + " : ", time1, Microsecond);
         }
         log.write("Finished in : " + timer.stop());
     }
